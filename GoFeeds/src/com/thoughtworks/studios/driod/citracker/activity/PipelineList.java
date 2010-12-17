6ed2366ce83dc353f9738a5cc53a64d18ddd0d0c
Anandha Krishnan <anandhak@gmail.com>
2010-12-17 15:16:40 +0530

    124570-7571341: Coarse way of refereshing need to optimize by making better use of ListAdapters

--- GoFeeds/src/com/thoughtworks/studios/driod/citracker/activity/PipelineList.java
+++ GoFeeds/src/com/thoughtworks/studios/driod/citracker/activity/PipelineList.java
@@ -1,8 +1,5 @@
 package com.thoughtworks.studios.driod.citracker.activity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,13 +8,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
-import android.widget.Toast;
-
 import com.thoughtworks.studios.driod.citracker.FeedParserFactory;
 import com.thoughtworks.studios.driod.citracker.MainMenuOptions;
 import com.thoughtworks.studios.driod.citracker.R;
 import com.thoughtworks.studios.driod.citracker.model.Pipeline;
 import com.thoughtworks.studios.driod.citracker.view.PipelineListAdapter;
+
+import java.util.List;
 
 /**
  * Understands named pipeline and show them with the information from the
@@ -25,21 +22,24 @@ import com.thoughtworks.studios.driod.citracker.view.PipelineListAdapter;
  */
 public class PipelineList extends ListActivity {
 	private List<Pipeline> pipelines;
-
-	public static String feedUrl = "http://go02.thoughtworks.com:8153/go/api/pipelines/%s/stages.xml";
-	public static final String PIPELINE_1 = "cruise";
-	public static final String PIPELINE_2 = "acceptance";
 	public static final String SELECTED_PIPELINE_URL_KEY = "com.thoughtworks.studios.driod.citracker.activity.CurrentPipelineName";
 
 	@Override
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
-		setContentView(com.thoughtworks.studios.driod.citracker.R.layout.main);
+		setContentView(R.layout.main);
 		pipelines = FeedParserFactory.loadPipelines(this);
 		this.setListAdapter(new PipelineListAdapter(this, R.layout.row, pipelines));
 	}
 
-	@Override
+    @Override
+    protected void onResume() {
+        super.onResume();
+        pipelines = FeedParserFactory.loadPipelines(this);
+        this.setListAdapter(new PipelineListAdapter(this, R.layout.row, pipelines));
+    }
+
+    @Override
 	public void onContentChanged() {
 		super.onContentChanged();
 		pipelines = FeedParserFactory.loadPipelines(this);
@@ -49,10 +49,8 @@ public class PipelineList extends ListActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
-		menu.add(Menu.NONE, MainMenuOptions.ADD_PIPELINE.ordinal(), MainMenuOptions.ADD_PIPELINE.ordinal(),
-				com.thoughtworks.studios.driod.citracker.R.string.add_pipeline);
-		menu.add(Menu.NONE, MainMenuOptions.PREFERENCES.ordinal(), MainMenuOptions.PREFERENCES.ordinal(),
-				com.thoughtworks.studios.driod.citracker.R.string.preferences);
+		menu.add(Menu.NONE, MainMenuOptions.ADD_PIPELINE.ordinal(), MainMenuOptions.ADD_PIPELINE.ordinal(),R.string.add_pipeline);
+		menu.add(Menu.NONE, MainMenuOptions.PREFERENCES.ordinal(), MainMenuOptions.PREFERENCES.ordinal(),R.string.preferences);
 		return true;
 	}
 
--- GoFeeds/src/com/thoughtworks/studios/driod/citracker/activity/PreferencesFromCode.java
+++ GoFeeds/src/com/thoughtworks/studios/driod/citracker/activity/PreferencesFromCode.java
@@ -7,7 +7,6 @@ import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.text.method.PasswordTransformationMethod;
 import android.text.method.SingleLineTransformationMethod;
-import android.widget.Toast;
 import com.thoughtworks.studios.driod.citracker.R;
 
 public class PreferencesFromCode extends PreferenceActivity {
@@ -22,7 +21,7 @@ public class PreferencesFromCode extends PreferenceActivity {
     @Override
 	protected void onStop() {
 		super.onStop();
-		Toast.makeText(getApplicationContext(), "Saving configuration...", Toast.LENGTH_SHORT).show();
+//		Toast.makeText(getApplicationContext(), "Saving configuration...", Toast.LENGTH_SHORT).show();
 	}
 
 
@@ -40,14 +39,6 @@ public class PreferencesFromCode extends PreferenceActivity {
 		serverUrl.setKey("serverUrl");
         serverUrl.setTitle(R.string.server_base_url);
         inlinePrefCat.addPreference(serverUrl);
-        
-//TODO Enable Auth on UI based on checkbox to be implemented below
-        // Toggle preference
-//        CheckBoxPreference togglePref = new CheckBoxPreference(this);
-//        togglePref.setKey("toggle_preference");
-//        togglePref.setTitle(R.string.title_toggle_preference);
-//        togglePref.setSummary(R.string.summary_toggle_preference);
-//        inlinePrefCat.addPreference(togglePref);
 
         PreferenceCategory dialogBasedPrefCat = new PreferenceCategory(this);
         dialogBasedPrefCat.setTitle(R.string.credentials);
@@ -77,68 +68,6 @@ public class PreferencesFromCode extends PreferenceActivity {
         pipelineName.setTitle(R.string.pipeline);
         pipelineName.setSummary(R.string.pipelines_example);
         dialogBasedPrefCat.addPreference(pipelineName);
-
-        // Launch preferences
-//        PreferenceCategory launchPrefCat = new PreferenceCategory(this);
-//        launchPrefCat.setTitle(R.string.launch_preferences);
-//        root.addPreference(launchPrefCat);
-
-        /*
-         * The Preferences screenPref serves as a screen break (similar to page
-         * break in word processing). Like for other preference types, we assign
-         * a key here so that it is able to save and restore its instance state.
-         */
-        // Screen preference
-//        PreferenceScreen screenPref = getPreferenceManager().createPreferenceScreen(this);
-//        screenPref.setKey("screen_preference");
-//        screenPref.setTitle(R.string.title_screen_preference);
-//        screenPref.setSummary(R.string.summary_screen_preference);
-//        launchPrefCat.addPreference(screenPref);
-
-        /*
-         * You can add more preferences to screenPref that will be shown on the
-         * next screen.
-         */
-
-//        // Example of next screen toggle preference
-//        CheckBoxPreference nextScreenCheckBoxPref = new CheckBoxPreference(this);
-//        nextScreenCheckBoxPref.setKey("next_screen_toggle_preference");
-//        nextScreenCheckBoxPref.setTitle(R.string.title_next_screen_toggle_preference);
-//        nextScreenCheckBoxPref.setSummary(R.string.summary_next_screen_toggle_preference);
-//        screenPref.addPreference(nextScreenCheckBoxPref);
-//
-//        // Intent preference
-//        PreferenceScreen intentPref = getPreferenceManager().createPreferenceScreen(this);
-//        intentPref.setIntent(new Intent().setAction(Intent.ACTION_VIEW)
-//                .setData(Uri.parse("http://www.android.com")));
-//        intentPref.setTitle(R.string.title_intent_preference);
-//        intentPref.setSummary(R.string.summary_intent_preference);
-//        launchPrefCat.addPreference(intentPref);
-//
-//        // Preference attributes
-//        PreferenceCategory prefAttrsCat = new PreferenceCategory(this);
-//        prefAttrsCat.setTitle(R.string.preference_attributes);
-//        root.addPreference(prefAttrsCat);
-//
-//        // Visual parent toggle preference
-//        CheckBoxPreference parentCheckBoxPref = new CheckBoxPreference(this);
-//        parentCheckBoxPref.setTitle(R.string.title_parent_preference);
-//        parentCheckBoxPref.setSummary(R.string.summary_parent_preference);
-//        prefAttrsCat.addPreference(parentCheckBoxPref);
-//
-//        // Visual child toggle preference
-//        // See res/values/attrs.xml for the <declare-styleable> that defines
-//        // TogglePrefAttrs.
-//        TypedArray a = obtainStyledAttributes(R.styleable.TogglePrefAttrs);
-//        CheckBoxPreference childCheckBoxPref = new CheckBoxPreference(this);
-//        childCheckBoxPref.setTitle(R.string.title_child_preference);
-//        childCheckBoxPref.setSummary(R.string.summary_child_preference);
-//        childCheckBoxPref.setLayoutResource(
-//                a.getResourceId(R.styleable.TogglePrefAttrs_android_preferenceLayoutChild,
-//                        0));
-//        prefAttrsCat.addPreference(childCheckBoxPref);
-//        a.recycle();
-
-        return root;
+       return root;
     }
 }
